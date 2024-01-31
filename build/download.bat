c8598fc781e422c9c17f1f7b2414ba46ddd3d8d8
SimeonTheCoder <69637412+SimeonTheCoder@users.noreply.github.com>
2024-01-31 12:22:16 +0200

    Small fix

--- build/download.bat
+++ build/download.bat
@@ -1,3 +1,3 @@
 curl -o NLang.jar https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/NLang.jar
 curl -o nlang.bat https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/nlang.bat
-curl -o nlang.bat https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/build.bat
+curl -o build.bat https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/build.bat
