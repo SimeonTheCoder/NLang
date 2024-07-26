2c151eb775d8b490a167a7da63c1f82e14968c9a
Simeon Petkov <petkov.moni@gmail.com>
2024-07-26 15:43:32 +0300

    Fixed broken download

--- build/build.bat
+++
@@ -1,5 +0,0 @@
-cd build
-%JAVA_HOME%/bin/javac CustomOperation.java
-%JAVA_HOME%/bin/javac CustomOperation.java
-%JAVA_HOME%/bin/javac CustomOperation.java
-%JAVA_HOME%/bin/javac CustomOperation.java
--- build/download.bat
+++ build/download.bat
@@ -2,5 +2,5 @@ curl -o NLang.jar https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/bu
 curl -o nlang.bat https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/nlang.bat
 curl -o build.bat https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/build.bat
 curl -o CustomOperation.class https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation.class
-curl -o CustomOperation$1.class https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation$1.class
-curl -o CustomOperation$Window.class https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation$Window.class
+curl -o "CustomOperation$1.class" https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation%241.class
+curl -o "CustomOperation$Window.class" https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation%24Window.class
