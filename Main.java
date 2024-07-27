ac67475608c440368bb1ad47da441b44743655f5
Simeon Petkov <petkov.moni@gmail.com>
2024-07-27 16:13:33 +0300

    Added exec command to execute programs from files in multithreaded mode even from interpreter mode

--- Main.java
+++ Main.java
@@ -9,102 +9,108 @@ import transformer.ProgramTransformer;
 import utils.EnumUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
 public class Main {
+    public static void executeFromFile(String[] args) throws ClassNotFoundException, FileNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException, InterruptedException {
+        int debug = -1;
+
+        EnumUtils.initClass();
+
+        float[] memory = new float[MemoryManager.TOTAL_AMOUNT];
+
+        int toSkip = 1;
+        int toReadArg = 0;
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].charAt(0) == '-') {
+                int val = Integer.parseInt(args[i].substring(2));
+
+                switch (args[i].charAt(1)) {
+                    case 'G' -> MemoryManager.GLOBAL_AMOUNT = val;
+                    case 'L' -> MemoryManager.LOCAL_AMOUNT = val;
+                    case 'T' -> MemoryManager.TOTAL_AMOUNT = val;
+                    case 'N' -> MemoryManager.NODE_SLOTS = val;
+                    case 'D' -> debug = val;
+                }
+
+                toSkip++;
+            } else {
+                toReadArg = i;
+            }
+        }
+
+        Parser parser = new Parser();
+        Node mainNode = parser.parse(new File(args[toReadArg] + ".nlp"), memory);
+
+        Linker linker = new Linker();
+
+        linker.linkArgs(
+                Arrays.stream(args).skip(toSkip).map(
+                        Float::parseFloat
+                ).toList().toArray(new Float[]{}),
+                memory
+        );
+
+        ThreadManager threadManager = new ThreadManager(8, memory);
+        threadManager.enqueueNode(mainNode, 0, 0);
+
+        long start = System.currentTimeMillis();
+
+        threadManager.startAll();
+
+        for (int i = 0; i < threadManager.threads.length; i++) {
+            threadManager.threads[i].join();
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+
+        if (debug == 0) {
+            for (int i = 0; i < MemoryManager.LOCAL_AMOUNT; i++) {
+                if (memory[i] == 0f) continue;
+                System.out.println("$" + i + " -> " + memory[i]);
+            }
+            for (int i = MemoryManager.LOCAL_AMOUNT; i < MemoryManager.TOTAL_AMOUNT; i++) {
+                if (memory[i] == 0f) continue;
+                System.out.println("$" + i + " (%" + (i - MemoryManager.LOCAL_AMOUNT) + ") -> " + memory[i]);
+            }
+        } else if (debug == 1) {
+            for (int i = 0; i < parser.nodesArr.length; i++) {
+                if (parser.nodesArr[i] == null) continue;
+                System.out.println(i + " -> " + parser.nodesArr[i].instruction[0]);
+            }
+        } else if (debug == 2) {
+            Scanner scanner = new Scanner(new File(args[toReadArg] + ".nlp"));
+
+            List<String> lines = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                lines.add(line);
+            }
+
+            ProgramTransformer.transform(lines);
+
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         MemoryManager.set(2048, 128, 2, 1024);
 
         try {
-            int debug = -1;
-
             if (args.length != 0 && !args[0].startsWith("--")) {
-                EnumUtils.initClass();
-
-                float[] memory = new float[MemoryManager.TOTAL_AMOUNT];
-
-                int toSkip = 1;
-                int toReadArg = 0;
-
-                for (int i = 0; i < args.length; i ++) {
-                    if (args[i].charAt(0) == '-') {
-                        int val = Integer.parseInt(args[i].substring(2));
-
-                        switch (args[i].charAt(1)) {
-                            case 'G' -> MemoryManager.GLOBAL_AMOUNT = val;
-                            case 'L' -> MemoryManager.LOCAL_AMOUNT = val;
-                            case 'T' -> MemoryManager.TOTAL_AMOUNT = val;
-                            case 'N' -> MemoryManager.NODE_SLOTS = val;
-                            case 'D' -> debug = val;
-                        }
-
-                        toSkip++;
-                    } else {
-                        toReadArg = i;
-                    }
-                }
-
-                Parser parser = new Parser();
-                Node mainNode = parser.parse(new File(args[toReadArg] + ".nlp"), memory);
-
-                Linker linker = new Linker();
-
-                linker.linkArgs(
-                        Arrays.stream(args).skip(toSkip).map(
-                                Float::parseFloat
-                        ).toList().toArray(new Float[]{}),
-                        memory
-                );
-
-                ThreadManager threadManager = new ThreadManager(8, memory);
-                threadManager.enqueueNode(mainNode, 0, 0);
-
-                long start = System.currentTimeMillis();
-
-                threadManager.startAll();
-
-                for(int i = 0; i < threadManager.threads.length; i ++) {
-                    threadManager.threads[i].join();
-                }
-
-                long end = System.currentTimeMillis();
-                System.out.println(end - start);
-
-                if(debug == 0) {
-                    for (int i = 0; i < MemoryManager.LOCAL_AMOUNT; i ++) {
-                        if(memory[i] == 0f) continue;
-                        System.out.println("$" + i + " -> " + memory[i]);
-                    }
-                    for (int i = MemoryManager.LOCAL_AMOUNT; i < MemoryManager.TOTAL_AMOUNT; i ++) {
-                        if(memory[i] == 0f) continue;
-                        System.out.println("$" + i + " (%" + (i - MemoryManager.LOCAL_AMOUNT) + ") -> " + memory[i]);
-                    }
-                } else if (debug == 1) {
-                    for (int i = 0; i < parser.nodesArr.length; i ++) {
-                        if(parser.nodesArr[i] == null) continue;
-                        System.out.println(i + " -> " + parser.nodesArr[i].instruction[0]);
-                    }
-                } else if (debug == 2) {
-                    Scanner scanner = new Scanner(new File(args[toReadArg] + ".nlp"));
-
-                    List<String> lines = new ArrayList<>();
-
-                    while (scanner.hasNextLine()) {
-                        String line = scanner.nextLine();
-                        lines.add(line);
-                    }
-
-                    ProgramTransformer.transform(lines);
-
-                    for (String line : lines) {
-                        System.out.println(line);
-                    }
-                }
+                executeFromFile(args);
             } else {
-                if(args.length != 0 && args[0].startsWith("--")) {
+                if (args.length != 0 && args[0].startsWith("--")) {
                     if (args[0].equals("--get")) {
                         File theDir = new File(System.getProperty("user.dir") + "/build");
-                        if (!theDir.exists()){
+                        if (!theDir.exists()) {
                             theDir.mkdirs();
                         }
 
@@ -120,7 +126,7 @@ public class Main {
 
                         Scanner scanner = new Scanner(new File("package.nlb"));
 
-                        while(scanner.hasNextLine()) {
+                        while (scanner.hasNextLine()) {
                             String line = scanner.nextLine();
 
                             String fileLocation = String.format(
@@ -154,7 +160,7 @@ public class Main {
                     String line = scanner.nextLine();
 
                     while (!line.equals("exit")) {
-                        if (line.startsWith("args")){
+                        if (line.startsWith("args")) {
                             String[] tokens = line.split(" ");
 
                             Operation operation = EnumUtils.getOperation(tokens[1].toUpperCase());
@@ -175,6 +181,8 @@ public class Main {
                             });
 
                             System.out.println(")\n" + operation.help());
+                        } else if (line.startsWith("exec")) {
+                            executeFromFile(line.substring(4).trim().split(" "));
                         } else if (line.chars().allMatch(Character::isDigit)) {
                             Node node = new Node();
                             node.id = nodeId++;
@@ -203,14 +211,14 @@ public class Main {
                         } else {
                             boolean includeZero = line.endsWith("+0");
 
-                            if(line.contains("local")) {
-                                for (int i = 0; i < MemoryManager.LOCAL_AMOUNT; i ++) {
-                                    if(memory[i] == 0f && !includeZero) continue;
+                            if (line.contains("local")) {
+                                for (int i = 0; i < MemoryManager.LOCAL_AMOUNT; i++) {
+                                    if (memory[i] == 0f && !includeZero) continue;
                                     System.out.println("$" + i + " -> " + memory[i]);
                                 }
                             } else if (line.contains("global")) {
-                                for (int i = MemoryManager.LOCAL_AMOUNT; i < MemoryManager.TOTAL_AMOUNT; i ++) {
-                                    if(memory[i] == 0f && !includeZero) continue;
+                                for (int i = MemoryManager.LOCAL_AMOUNT; i < MemoryManager.TOTAL_AMOUNT; i++) {
+                                    if (memory[i] == 0f && !includeZero) continue;
                                     System.out.println("$" + i + " (%" + (i - MemoryManager.LOCAL_AMOUNT) + ") -> " + memory[i]);
                                 }
                             }
@@ -221,7 +229,7 @@ public class Main {
                     }
                 }
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
