837acdbc58c093790c5fd4262e19c9e269fc6b42
Simeon Petkov <petkov.moni@gmail.com>
2024-07-27 21:04:20 +0300

    Re-fixed download.bat

--- build/download.bat
+++ build/download.bat
@@ -1,6 +1,7 @@
+mkdir build
 curl -o NLang.jar https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/NLang.jar
 curl -o nlang.bat https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/nlang.bat
 curl -o build.bat https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/build.bat
-curl -o build/CustomOperation.class https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation.class
-curl -o "build/CustomOperation$1.class" https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation%241.class
-curl -o "build/CustomOperation$Window.class" https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation%24Window.class
+curl -o build/CustomOperation.class "https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation.class"
+curl -o "build/CustomOperation$1.class" "https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation$1.class"
+curl -o "build/CustomOperation$Window.class" "https://raw.githubusercontent.com/SimeonTheCoder/NLang/main/build/CustomOperation$Window.class"
